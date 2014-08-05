3772ed0e158a7add8acd14b55affcc00c7ce27fd
ruanyl <ruanyu1@gmail.com>
2014-08-05 22:17:55 +0300

    notice now has position

--- purenotify.js
+++ purenotify.js
@@ -5,22 +5,39 @@
 (function($) {
     var defaults = {
         bgColor: '#000',
-        offsetX: 10,
-        offsetY: 10,
+        offsetX: "10",
+        offsetY: "10",
+        opacity: 0.6,
+        position: 'bottom-right',
         fontColor: '#fff',
         msg: 'Notice!'
     };
-    var inner = 'position: absolute; top: 10px; left: 10px; padding: 10px 15px; background-color: #000; color: #fff; opacity: 0.6; filter: alpha(opacity=60); /* For IE8 and earlier */ z-index: 999;';
+    var inner = 'position: absolute; padding: 10px 15px; z-index: 999;';
     var wrapper = 'position: absolute; display: inline-block; height: 0; width: 0; z-index: 1;';
     $.fn['notice'] = function(options) {
-        var notification_html, custom_style, notice;
+        var notification_html,
+            custom_style,
+            notice,
+            top,
+            right,
+            bottom,
+            left;
+
         options = $.extend(true, {}, defaults, options);
-        custom_style = 'top:' + options.offsetY + 'px;left:' + options.offsetX + 'px;background-color:' + options.bgColor + ';color:' + options.fontColor + ';';
+        top = options.position.indexOf('top') >= 0 ? 'top:' + options.offsetY + 'px;' : '';
+        right = options.position.indexOf('right') >= 0 ? 'right:' + (options.offsetX - this.width()) + 'px;' : '';
+        bottom = options.position.indexOf('bottom') >= 0 ? 'bottom:' + (options.offsetY - this.height()) + 'px;' : '';
+        left = options.position.indexOf('left') >= 0 ? 'left:' + options.offsetY + 'px;' : '';
+        custom_style = top + right + bottom + left +
+            'background-color:' + options.bgColor +
+            ';color:' + options.fontColor +
+            ';opacity:' + options.opacity +
+            ';filter:alpha(opacity=' + (options.opacity * 100) + ');';
         notice = this.find('#notice');
-        if(notice.length !== 0) {
+        if (notice.length !== 0) {
             notice.remove();
         }
-        notification_html = '<div class="notice" style="' + wrapper + '"><div style="' + inner +  custom_style + '">' + options.msg + '</div></div>';
+        notification_html = $('<div class="notice" style="' + wrapper + '"><div style="' + inner + custom_style + '">' + options.msg + '</div></div>');
         this.prepend(notification_html);
         return this;
     };
